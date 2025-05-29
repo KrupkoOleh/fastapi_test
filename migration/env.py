ec3c41118a0239f1304f4bf7fd08d100d216a7f7
Oleh Krupko <oleh.krupko@hneu.net>
2025-05-29 12:44:22 +0300

    creating first migration

---
+++ migration/env.py
@@ -0,0 +1,58 @@
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncConnection
+
+from database import Base
+
+target_metadata = Base.metadata
+
+config = context.config
+
+# Установка логирования
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+
+def run_migrations_offline() -> None:
+    """Запуск миграций в оффлайн-режиме."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: AsyncConnection) -> None:
+    """Выполнение миграций."""
+    context.configure(
+        connection=connection, target_metadata=target_metadata
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Запуск миграций в онлайн-режиме."""
+    connectable = create_async_engine(
+        config.get_main_option("sqlalchemy.url"), poolclass=pool.NullPool
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    import asyncio
+    asyncio.run(run_migrations_online())
